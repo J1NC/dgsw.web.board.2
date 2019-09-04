efbef09c71253640aff64c6772c23244e9db9b1b
J1NC <womin1120@naver.com>
2019-09-04 19:14:14 +0900

    [ADD] 어 라우터 빼먹음

--- backend/routes/board/index.js
+++ backend/routes/board/index.js
@@ -2,5 +2,5 @@ const router = require('express').Router();
 const controller = require('./board.controller');
 
 router.get('/', controller.list);
-
+router.post('/', controller.create);
 module.exports = router;
